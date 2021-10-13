92d724b8635fbd36977cf0a102f05378867c4f19
Hermo Terblanche <hermo.terblanche@partner.bmw.co.za>
2021-10-13 11:47:22 +0200

    Removed obsolete method

--- src/Infrastructure/Kafka/Repositories/KafkaRepository-Subscriptions.cs
+++ src/Infrastructure/Kafka/Repositories/KafkaRepository-Subscriptions.cs
@@ -79,15 +79,6 @@ namespace OMP.Connector.Infrastructure.Kafka.Repositories
             return (hasRemoved && persisted);
         }
 
-        public bool ClearRepository()
-        {
-            _managedSubscriptions.Clear();
-            _endpointDescriptions.Clear();
-            var persisted = PersistCachedConfig();
-
-            return persisted;
-        }
-
         private void WaitForInitializationToComplete()
         {
             while (!_repositoryInitialized)
