60964eccedec66c1396be718d931973fb7c21a8d
Collen Thobela <collen.thobela@partner.bmw.co.za>
2022-05-06 11:56:07 +0200

    Fix: compare discovered endpointUrl with configured server url endpointUrl

--- src/Infrastructure/OpcUa/UserIdentityProvider.cs
+++ src/Infrastructure/OpcUa/UserIdentityProvider.cs
@@ -2,6 +2,7 @@
 // Copyright Contributors to the Open Manufacturing Platform.
 
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Extensions.Options;
 using OMP.Connector.Domain.Configuration;
 using OMP.Connector.Domain.OpcUa;
@@ -12,21 +13,40 @@ namespace OMP.Connector.Infrastructure.OpcUa
     public class UserIdentityProvider : IUserIdentityProvider
     {
         private readonly List<AuthenticationConfiguration> _authenticationSettings;
-        
+
         public UserIdentityProvider(
             IOptions<ConnectorConfiguration> connectorConfiguration
             )
         {
             _authenticationSettings = connectorConfiguration.Value.OpcUa.Authentication;
         }
-        
+
         public IUserIdentity GetUserIdentity(EndpointDescription endpointDescription)
         {
             if (endpointDescription.SecurityMode < MessageSecurityMode.SignAndEncrypt)
                 return new UserIdentity();
-            
-            var setting = _authenticationSettings?.Find(x => x.Endpoint.Equals(endpointDescription.EndpointUrl));
+
+            var setting = _authenticationSettings?.Find(s =>
+            {
+                var endpointCollection = GetEndpoints(s.Endpoint);
+                var endpointDescriptions = endpointCollection.OrderByDescending(e => e.SecurityLevel);
+
+                var endpoint = endpointDescriptions?.FirstOrDefault(x =>
+                        x.EndpointUrl.Equals(endpointDescription.EndpointUrl) &&
+                        x.SecurityMode.Equals(endpointDescription.SecurityMode));
+
+                return endpoint != null;
+            });
+
             return setting is null ? new UserIdentity() : new UserIdentity(setting.Username, setting.Password);
+        }
+
+        private EndpointDescriptionCollection GetEndpoints(string endpoint)
+        {
+            var uri = new System.Uri(endpoint);
+            var client = DiscoveryClient.Create(uri);
+            var endpointCollection = client.GetEndpoints(default);
+            return endpointCollection;
         }
     }
 }
