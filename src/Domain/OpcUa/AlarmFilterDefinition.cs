5e3aef8eadc10ccd816b38f8b693b89ba543aeb6
Hermo Terblanche <hermo.terblanche@partner.bmw.co.za>
2022-06-24 12:31:51 +0200

    Added back original license header and removed unused utilities

--- src/Domain/OpcUa/AlarmFilterDefinition.cs
+++ src/Domain/OpcUa/AlarmFilterDefinition.cs
@@ -1,5 +1,31 @@
-﻿// SPDX-License-Identifier: MIT. 
-// Copyright Contributors to the Open Manufacturing Platform.
+﻿/* ========================================================================
+ * Copyright (c) 2005-2019 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
 
 using System;
 using System.Collections.Generic;
@@ -219,7 +245,7 @@ namespace OMP.Connector.Domain.OpcUa
         private void CollectFields(Session session, NodeId eventTypeId, SimpleAttributeOperandCollection eventFields)
         {
             // get the supertypes.
-            ReferenceDescriptionCollection supertypes = FormUtils.BrowseSuperTypes(session, eventTypeId, false);
+            ReferenceDescriptionCollection supertypes = Utilities.BrowseSuperTypes(session, eventTypeId, false);
 
             if (supertypes == null)
             {
@@ -264,7 +290,7 @@ namespace OMP.Connector.Domain.OpcUa
             nodeToBrowse.NodeClassMask = (uint)(NodeClass.Object | NodeClass.Variable);
             nodeToBrowse.ResultMask = (uint)BrowseResultMask.All;
 
-            ReferenceDescriptionCollection children = FormUtils.Browse(session, nodeToBrowse, false);
+            ReferenceDescriptionCollection children = Utilities.Browse(session, nodeToBrowse, false);
 
             if (children == null)
             {
